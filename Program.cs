86f27acac5d092e0d1a056e04bcb9d39440644e3
Ismael Dias <ismaelmata200@hotmail.com>
2021-12-11 02:29:54 -0300

    projeto iniciado ola mundo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = DateTime.Now;
+
+            Console.WriteLine($"{data.Date.ToString("dd/MM/yyyy")}");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
